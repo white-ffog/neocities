8d718d82d9b21fcfe3f38fe00e0ab980c2a08758
GusGurgel <gurgelg15@gmail.com>
2025-08-16 11:12:34 -0300

    Changing about folder. Updating links.

--- public/scripts/init.js
+++ public/scripts/init.js
@@ -6,7 +6,7 @@ const header_html = `
 <a href="/dummy.html">🕳️Dummy</a><span class="no-select"> | </span>
 <a href="/">🔍Entradas</a><span class="no-select"> | </span>
 <a href="/">🖼️Galeria</a><span class="no-select"> | </span>
-<a href="/about/index.html">🤔Sobre</a>
+<a href="/about.html">🤔Sobre</a>
 <hr />
 `
 
