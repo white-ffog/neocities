bdee44222c586f0a7a05f8ee24ab1a2682c7a0c9
GusGurgel <gurgelg15@gmail.com>
2026-03-18 18:08:50 -0300

    FEAT: Returning the neocities site.

--- public/scripts/init.js
+++ public/scripts/init.js
@@ -1,20 +1,18 @@
 const header_html = `
-<h1 id="logo"> WhiteFog 🌁</h1>
+<h1 id="logo"> WhiteFog </h1>
 
 <nav id="nav-menu">
-<a href="/">🏡Início</a><span class="no-select"> | </span>
-<a href="/entries.html">🔍Entradas</a><span class="no-select"> | </span>
-<a href="/wiki.html">📖Wiki</a><span class="no-select"> | </span>
-<a href="/rabbit-holes.html">🕳️🐰 Rabbit Holes</a><span class="no-select"> | </span>
-<a href="/about.html">🤔Sobre</a>
+<a href="/">Início</a><span class="no-select"> | </span>
+<a href="/blogs.html">Blogs</a><span class="no-select"> | </span>
+<a href="/about.html">Sobre</a>
 <hr />
 `
 
 const footer_html = `
 <hr />
-<div><em id="regard">"Esse site é feito por um humano. Logo, como todo
+<div id="regard"><em>"Esse site é feito por um humano. Logo, como todo
 humano, ele vai errar. Sempre confirme as informações
-em outras fontes"</em></div>
+em outras fontes"<em></div>
 <div id="trademark"> © WhiteFog, 2025 | Contato: dopauarthur@gmail.com<div/>
 `
 
